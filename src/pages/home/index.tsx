029c5d24155fab193997baac1a276be99e334a5b
gopine <xliiauo25@gmail.com>
2025-02-24 16:57:43 +0800

    feat: add page build content

--- src/pages/home/index.tsx
+++
@@ -1,7 +0,0 @@
-import MonthView from '@/pages/home/month-view';
-
-function Home() {
-	return <MonthView />;
-}
-
-export default Home;
--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface GithubContent {
-	name: string;
-	path: string;
-	html_url: string;
-	url: string;
-	sha?: string;
-	content?: string;
-	encoding: string;
-}
